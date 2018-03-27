74a58ee87e29a692ac60ba6148cb8b20a7cce02e
Rodrigo Waltenberg <rodrigo.waltenberg@beautydate.com>
2018-03-27 11:05:16 -0300

    Adiciona os scripts em closures

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,28 @@
-angular.module('myApp', ['io.service']).
+(function(){
+  angular.module('myApp', ['io.service'])
 
-run(function (io) {
-  io.init({
-    ioServer: 'http://localhost:3696',
-  });
+  .run(function (io) {
+    io.init({
+      ioServer: 'http://localhost:3696',
+    });
 
-  moment.locale(navigator.language);
-}).
+    moment.locale(navigator.language);
+  })
 
-controller('MainController', function ($scope, io) {
-  $scope.send = function () {
-    if (!$scope.message.trim()) {
-      return
+  .controller('MainController', function ($scope, io) {
+    $scope.send = function () {
+      if (!$scope.message.trim()) {
+        return
+      }
+
+      io.emit($scope.message);
+      $scope.message = null;
     }
 
-    io.emit($scope.message);
-    $scope.message = null;
-  }
-
-  io.watch('message', function (data) {
-    data.time = moment(data.time).format('L LT')
-    $scope.lastMessage = data;
-    $scope.$apply();
-  });
-});
+    io.watch('message', function (data) {
+      data.time = moment(data.time).format('L LT')
+      $scope.lastMessage = data;
+      $scope.$apply();
+    });
+  })
+})();
