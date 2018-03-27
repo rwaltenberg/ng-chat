47c8e6321a69597a953fcbe9637120210000c440
Rodrigo Waltenberg <rodrigo.waltenberg@beautydate.com>
2018-03-27 11:36:03 -0300

    Encapsulating main app on directive

--- app/js/app.js
+++ app/js/app.js
@@ -9,20 +9,30 @@
     moment.locale(navigator.language);
   })
 
-  .controller('MainController', function ($scope, io) {
-    $scope.send = function () {
-      if (!$scope.message.trim()) {
-        return
       }
-
-      io.emit($scope.message);
-      $scope.message = null;
     }
 
-    io.watch('message', function (data) {
-      data.time = moment(data.time).format('L LT')
-      $scope.lastMessage = data;
-      $scope.$apply();
-    });
-  })
+  .directive('chat', ['$rootScope', 'io', function ($rootScope, io) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/chat.html',
+      scope: {},
+      link: function (scope) {
+        scope.send = function () {
+          if (!scope.message.trim()) {
+            return
+          }
+
+          io.emit(scope.message);
+          scope.message = null;
+        }
+
+        io.watch('message', function (data) {
+          data.time = moment(data.time).format('L LT')
+          scope.lastMessage = data;
+          scope.$apply();
+        });
+      }
+    }
+  }])
 })();
